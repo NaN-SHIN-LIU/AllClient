98ac7bb3a9834b0871a2aa05599f58a3fe6812a2
liu <11661642@qq.com>
2018-09-15 16:21:30 +0800

    1122334

--- src/main/java/cn/com/buyController/test.java
+++ src/main/java/cn/com/buyController/test.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class test {
     @RequestMapping("indexPage")
     public String indexPage(){
-        return "A";
+        System.out.println("aaaaaaaaaaa");return "A";
     }
     @RequestMapping("B")
     public String viewBpage(){
